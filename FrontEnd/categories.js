b87d129e7b7da78738ae2f0bd5a36ed9edbad6f4
Delphine Sa|mon Barrabes <delphinebarrabessalmon@gmail.com>
2023-10-19 16:48:57 +0200

    adding dynamic filters button

---
+++ FrontEnd/categories.js
@@ -0,0 +1,31 @@
+import { displayWorks } from "./works.js";
+
+// Récupération des projets depuis le ficher JSON//
+const reponse = await fetch('http://localhost:5678/api/categories');
+const categories = await reponse.json();
+
+//Affichage des filtres//
+const sectionFiltres = document.querySelector(".filters")
+
+displayFilter({name:"Tous"})
+
+for (const category of categories){
+    displayFilter(category)
+}
+
+function displayFilter(category){
+    const categoryElement = document.createElement("button")
+    categoryElement.innerText = category.name
+    // TODO Rajout class pour css 
+    categoryElement.classList.add('button-filters')
+    sectionFiltres.appendChild(categoryElement)
+    categoryElement.addEventListener("click", function(event){
+        displayWorks(category.id)
+
+    })
+}
+
+
+
+
+
--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,19 +1,25 @@
 // Récupération des projets depuis le ficher JSON//
 const reponse = await fetch('http://localhost:5678/api/works');
-const projets = await reponse.json();
+const works = await reponse.json();
 
-// Création des balises
-const sectionPotfolio = document.querySelector(".gallery")
-for (const article of projets){
-    const figureElement = document.createElement("figure")
-    const imageElement = document.createElement("img")
-    imageElement.src = article.imageUrl
-    const titleElement = document.createElement("figcaption")
-    titleElement.innerText = article.title
-    
-    //Rattachement de nos balises au DOM  
-    figureElement.appendChild(imageElement)
-    figureElement.appendChild(titleElement)
-    sectionPotfolio.appendChild(figureElement)
+// Création des projets
+export function displayWorks(categoryId){
+    const sectionPotfolio = document.querySelector(".gallery")
+    sectionPotfolio.innerHTML = ""
+    for (const work of works){
+        if (work.categoryId === categoryId || categoryId === undefined){
+            const figureElement = document.createElement("figure")
+            const imageElement = document.createElement("img")
+            imageElement.src = work.imageUrl
+            const titleElement = document.createElement("figcaption")
+            titleElement.innerText = work.title
+            
+            //Rattachement de nos balises au DOM  
+            figureElement.appendChild(imageElement)
+            figureElement.appendChild(titleElement)
+            sectionPotfolio.appendChild(figureElement)
+        }
+    }
 }
 
+displayWorks()
